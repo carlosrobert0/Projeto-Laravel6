1a9953c55a9ddd1683d561ac3f8355206fc5ad0a
carlosrobert0 <carlosroberto15755@gmail.com>
2020-11-24 20:38:28 -0300

    Rotas

--- app-laravel/routes/web.php
+++ app-laravel/routes/web.php
@@ -2,6 +2,60 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('redirect3', function () {
+    return redirect().route('url.name');
+});
+
+Route::get('/nome-url', function () {
+    return 'heyy hey hey';
+})->name('url.name');
+
+Route::view('/view', 'welcome');
+
+Route::redirect('/redirect1', '/redirect2');
+
+// Route::get('redirect1', function () {
+//     return redirect('/redirect2');
+// });
+
+Route::get('redirect2', function () {
+    return 'Redirect 02';
+});
+
+Route::get('/produtos/{idProduct?}/details', function ($idProduct = '') {
+    return "Produtos {$idProduct}";
+});
+
+Route::get('/categorias/{flag}/posts', function ($flag) {
+    return "Posts da categoria: {$flag}";
+});
+
+Route::get('/categorias/{flag}', function ($flag) {
+    return "Produtos da categoria: {$flag}";
+});
+
+Route::match(['post', 'get'], '/match', function() {
+    return 'Match';
+});
+
+Route::any('/any', function() {
+    return 'Any';
+});
+
+Route::get('/empresa', function () {
+    return 'sobre a empresa';
+});
+
+Route::get('/contato', function () {
+    return view('site.contact');
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,10 +67,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
+
