447df65f072a8ee0a6c5d890261495780bd87735
carlosrobert0 <carlosroberto15755@gmail.com>
2020-11-25 19:11:00 -0300

    Grupo de rotas

--- app-laravel/routes/web.php
+++ app-laravel/routes/web.php
@@ -2,6 +2,49 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/login', function () {
+    return 'login';
+})->name('login');
+
+// Route::middleware([])->group(function () {
+//     Route::prefix('admin')->group(function () {
+
+//         Route::namespace('Admin')->group(function () {
+
+//             Route::name('admin.')->group(function () {
+//                 Route::get('/dashboard', 'TesteController@teste')->name('dasboard');
+
+//                 Route::get('/financeiro', 'TesteController@teste')->name('financeiro');
+
+//                 Route::get('/produtos', 'TesteController@teste')->name('products');
+
+//                 Route::get('/', 'TesteController@teste')->name('home');
+
+//                 Route::get('/', function () {
+//                     return redirect()->route('admin.dashboard');
+//                 })->name('home');
+//             });
+//         });
+//     });
+// });
+
+Route::group([
+    'middleware' => [],
+    'prefix' => 'admin',
+    'namespace' => 'Admin',
+    'name' => 'admin.'
+], function () {
+    Route::get('/dashboard', 'TesteController@teste')->name('dasboard');
+
+    Route::get('/financeiro', 'TesteController@teste')->name('financeiro');
+
+    Route::get('/produtos', 'TesteController@teste')->name('products');
+
+    Route::get('/', function () {
+        return redirect()->route('admin.dashboard');
+    })->name('home');
+});
+
 Route::get('redirect3', function () {
     return redirect().route('url.name');
 });
