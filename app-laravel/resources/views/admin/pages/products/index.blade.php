84f8464d8052b63ed4ab5a15809467c44852fabd
carlosrobert0 <carlosroberto15755@gmail.com>
2020-11-27 10:34:27 -0300

    Estruturas de controle blade

--- app-laravel/resources/views/admin/pages/products/index.blade.php
+++ app-laravel/resources/views/admin/pages/products/index.blade.php
@@ -6,5 +6,52 @@
 
     <h1>exibindo os produtos</h1>
 
-    {{ $teste }}
+    @if ($teste === '123')
+        è igual
+    @elseif ($teste == 123)
+        é igual a 123
+    @else
+        é diferente
+    @endif
+
+    @unless ($teste === '123')
+        sadsdasd
+    @else
+        asdasdasd
+    @endunless
+
+    @isset($teste2)
+        <p>{{ $teste2 }}</p>
+    @endisset
+
+    @empty($teste3)
+        <p>Vazio...</p>
+    @endempty
+
+    @auth
+        <p>Autenticado</p>
+    @else
+        <p>Nao autenticado</p>
+    @endauth
+
+    @guest
+        <p>Nao autenticado</p>
+    @endguest
+
+    @switch($teste)
+        @case(1)
+            igual 1
+            @break
+        @case(2)
+            igual a 2
+            @break
+        @case(3)
+            igual a 3
+            @break
+        @case(123)
+            igual a 123
+            @break
+        @default
+            Default
+    @endswitch
 @endsection
