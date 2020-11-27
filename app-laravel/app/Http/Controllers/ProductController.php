cc8286c553ed07531cce1c92f758f2045f8d0126
carlosrobert0 <carlosroberto15755@gmail.com>
2020-11-27 01:00:11 -0300

    Controllers de CRUD

--- app-laravel/app/Http/Controllers/ProductController.php
+++ app-laravel/app/Http/Controllers/ProductController.php
@@ -6,13 +6,34 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+
     public function index() {
         $products = ['Product 01', 'Product 02', 'Product 03'];
 
         return $products;
     }
 
-    public function show($id) {
+    public function show($id = '') {
         return "exibindo o produto de id: {$id}";
+    }
+
+    public function create() {
+        return "Exibindo o form de cadastro de um novo produto";
+    }
+
+    public function edit($id) {
+        return "form para editar o produto: {$id}";
+    }
+
+    public function store() {
+        return "Cadastrando um novo produto";
+    }
+
+    public function update($id) {
+        return "Editando o produto: {$id}";
+    }
+
+    public function destroy($id) {
+        return "Editando o produto: {$id}";
     }
 }
